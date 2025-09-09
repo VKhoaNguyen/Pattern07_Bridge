a91e11cc9f172127f947934abde55e65f2abce7c
Khoa Nguyên <vknguyen.work@gmail.com>
2025-09-09 10:48:09 +0700

    Add files via upload

---
+++ Dpm225512-Bridge/Optimized/Optimized/IDataObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optimized
+{
+    public interface IDataObject<T>
+    {
+        void NextRecord();
+        void PriorRecord();
+        void AddRecord(T t);
+        void DeleteRecord(T t);
+        T GetCurrentRecord();
+        void ShowRecord();
+        void ShowAllRecords();
+    }
+
+
+}
---
+++ Dpm225512-Bridge/Optimized/Optimized/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+
+namespace Optimized
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Create RefinedAbstraction and set ConcreteImplementor
+            var customers = new Customers(
+                new CustomersData("Chicago"));
+
+            // Exercise the bridge
+            customers.Show();
+            customers.Next();
+            customers.Show();
+            customers.Next();
+            customers.Show();
+
+            customers.Add("Henry Velasquez");
+            customers.ShowAll();
+
+            ReadKey();
+        }
+    }
+}
